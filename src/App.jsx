2f74361c35f1eccc303d5a3311a74a988f06a5ed
Xephna <36911354+Xephna@users.noreply.github.com>
2023-02-20 22:27:06 +0800

    add clicker hide  toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 // import reactLogo from './assets/react.svg'
+import { useState } from 'react'
 import './App.css'
 import Clicker from './Clicker'
 import NameCard from './NameCard'
 
 function App() {
 
+  const [hasClicker, setHasClicker] = useState(true)
+
+  function toggleClicker(){
+    if(hasClicker===true){
+      setHasClicker(false)
+    } else if (hasClicker===false){
+      setHasClicker(true)
+    }
+  }
+
+  console.log(hasClicker);
+
   return <div>
-    <p class="para1">hello</p>
+    <p className="para1" id='welcome'>hello</p>
+    <button onClick={toggleClicker}>{hasClicker?'Hide':'Show'} Clicker</button>
     <NameCard />
-    <Clicker />
+    {hasClicker?<Clicker />:null}
   </div>
   
 }
--- src/Clicker.jsx
+++ src/Clicker.jsx
@@ -1,10 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import './Clicker.css'
 
 function Clicker(){
-    const [count, setCount] = useState(0)
-    return <div style={{border:"black 1px solid"}}>
+    const [count, setCount] = useState(parseInt(localStorage.getItem('count') ?? 0))
+
+    function increaseCount(){
+        setCount(count+1)
+        // console.log(count);
+    }
+
+    useEffect(()=>{
+        const savedCount = parseInt(localStorage.getItem('count') ?? 0)
+        console.log(savedCount);
+        setCount(savedCount)
+    }, [])
+
+    useEffect(()=>{
+        localStorage.setItem('count', count)
+    }, [count])
+    
+    return <div className='clicker'>
         <p>Count: {count}</p>
-        <button onClick={()=>setCount(count+1)}>Increase count</button>
+        <button onClick={increaseCount}>Increase count</button>
     </div>
 }
 
